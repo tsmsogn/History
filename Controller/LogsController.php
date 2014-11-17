f74440b53994054a14452d6bcff8a07977d40e84
tsmsogn <tsmsogn@gmail.com>
2014-11-17 10:48:29 +0900

    refactor

--- Controller/LogsController.php
+++ Controller/LogsController.php
@@ -1,6 +1,5 @@
 <?php
 App::uses('HistoryAppController', 'History.Controller');
-
 /**
  * Logs Controller
  *
@@ -10,56 +9,56 @@ App::uses('HistoryAppController', 'History.Controller');
  */
 class LogsController extends HistoryAppController {
 
-    /**
-     * Components
-     *
-     * @var array
-     */
-    public $components = array('Paginator', 'Session');
+/**
+ * Components
+ *
+ * @var array
+ */
+	public $components = array('Paginator', 'Session');
 
-    /**
-     * admin_index method
-     *
-     * @return void
-     */
-    public function admin_index() {
-        $this->Log->recursive = 0;
-        $this->set('logs', $this->Paginator->paginate());
-    }
+/**
+ * admin_index method
+ *
+ * @return void
+ */
+	public function admin_index() {
+		$this->Log->recursive = 0;
+		$this->set('logs', $this->Paginator->paginate());
+	}
 
-    /**
-     * admin_view method
-     *
-     * @throws NotFoundException
-     * @param string $id
-     * @return void
-     */
-    public function admin_view($id = null) {
-        if (!$this->Log->exists($id)) {
-            throw new NotFoundException(__('Invalid log'));
-        }
-        $options = array('conditions' => array('Log.' . $this->Log->primaryKey => $id));
-        $this->set('log', $this->Log->find('first', $options));
-    }
+/**
+ * admin_view method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function admin_view($id = null) {
+		if (!$this->Log->exists($id)) {
+			throw new NotFoundException(__('Invalid log'));
+		}
+		$options = array('conditions' => array('Log.' . $this->Log->primaryKey => $id));
+		$this->set('log', $this->Log->find('first', $options));
+	}
 
-    /**
-     * admin_delete method
-     *
-     * @throws NotFoundException
-     * @param string $id
-     * @return void
-     */
-    public function admin_delete($id = null) {
-        $this->Log->id = $id;
-        if (!$this->Log->exists()) {
-            throw new NotFoundException(__('Invalid log'));
-        }
-        $this->request->allowMethod('post', 'delete');
-        if ($this->Log->delete()) {
-            $this->Session->setFlash(__('The log has been deleted.'));
-        } else {
-            $this->Session->setFlash(__('The log could not be deleted. Please, try again.'));
-        }
-        return $this->redirect(array('action' => 'index'));
-    }
+/**
+ * admin_delete method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function admin_delete($id = null) {
+		$this->Log->id = $id;
+		if (!$this->Log->exists()) {
+			throw new NotFoundException(__('Invalid log'));
+		}
+		$this->request->allowMethod('post', 'delete');
+		if ($this->Log->delete()) {
+			$this->Session->setFlash(__('The log has been deleted.'));
+		} else {
+			$this->Session->setFlash(__('The log could not be deleted. Please, try again.'));
+		}
+		return $this->redirect(array('action' => 'index'));
+	}
 }
